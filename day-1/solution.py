8dcd5f71c4e4d15f3a3b3b943675f145cb1a9546
Bhavya Muni <bhavyamuni@outlook.com>
2022-12-02 19:48:41 -0500

    name changes

--- day-1/solution.py
+++ day-1/solution.py
@@ -2,9 +2,9 @@ max_o = -1
 with open("input_1.txt", "r") as f:
     lines = f.readlines()
     cur = 0
-    for l in lines:
-        if l.strip():
-            cur += int(l)
+    for line in lines:
+        if line.strip():
+            cur += int(line)
         else:
             max_o = max(max_o, cur)
             cur = 0
@@ -15,9 +15,9 @@ max_o = []
 with open("input_1.txt", "r") as f:
     lines = f.readlines()
     cur = 0
-    for l in lines:
-        if l.strip():
-            cur += int(l)
+    for line in lines:
+        if line.strip():
+            cur += int(line)
         else:
             max_o.append(cur)
             cur = 0
