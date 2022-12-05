8b60fc3db31d959dcc411d352a6d7e1786a756c8
Bhavya Muni <bhavyamuni@outlook.com>
2022-12-04 22:49:25 -0500

    day 4✅

--- README.md
+++ README.md
@@ -10,3 +10,9 @@
 
 - dict to map values of each outcome
 - conditions for test cases and totals
+
+### Day 3
+
+- check for intersection between sets
+- add up intersection from dict that maps char to its priority
+- for part 2 take 3 lines instead of 2 compartments and do the same
---
+++ day-4/solution.py
@@ -0,0 +1,37 @@
+def main():
+    with open("input.txt", "r") as f:
+        lines = f.readlines()
+        out = 0
+        for line in lines:
+            int1, int2 = line.split(",")[0], line.split(",")[1]
+
+            int1min, int1max = int(int1.split("-")[0]), int(int1.split("-")[1])
+            int2min, int2max = int(int2.split("-")[0]), int(int2.split("-")[1])
+
+            if (int1min >= int2min and int1max <= int2max) or (int2min >= int1min and int2max <= int1max):
+                out += 1
+
+    return out
+
+
+def main2():
+    with open("input.txt", "r") as f:
+        lines = f.readlines()
+        out = 0
+        for line in lines:
+            int1, int2 = line.split(",")[0], line.split(",")[1]
+
+            int1min, int1max = int(int1.split("-")[0]), int(int1.split("-")[1])
+            int2min, int2max = int(int2.split("-")[0]), int(int2.split("-")[1])
+
+            # if (int1min >= int2min and int1max <= int2max) or (int2min >= int1min and int2max <= int1max):
+            if (int1min >= int2min and int1min <= int2max) or (int1max >= int2min and int1max <= int2max) or (int1min >= int2min and int1max <= int2max) or (int2min >= int1min and int2max <= int1max):
+                # print(f"{int1min} {int2min}")
+                out += 1
+
+    return out
+
+
+if __name__ == "__main__":
+    print(main())
+    print(main2())
